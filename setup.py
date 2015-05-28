bf8581f81b4d079db79b6ef5104059646b7bffcc
Sanketh Mopuru <sanketh.mopuru@gmail.com>
2015-05-28 16:19:05 +0530

    Final packaging done

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst')) as f:
+	long_description = f.read()
+
+setup(
+	name='bendcode',
+	version='1.0.0',
+
+	description='Python module to decode Bencoded data',
+	long_description=long_description,
+	url='https://github.com/sanketh95/BenDcode',
+
+	author='Sanketh Mopuru',
+	author_email='sanketh.mopuru@gmail.com',
+
+	license='GPLv2',
+
+	keywords=['bencoding', 'bittorrent'],
+	packages=['bendcode'],
+
+
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+		'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+	]
+
+)
